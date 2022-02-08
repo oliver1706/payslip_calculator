70e2106ed62e940dac05353e45ac1a8a4f82c3bd
Oliver <oliver.vaningen@gmail.com>
2022-02-08 21:06:30 +1100

    Prevented the program from accepting negative income values

--- src/main/java/com/myob/payslip_calculator/Main.java
+++ src/main/java/com/myob/payslip_calculator/Main.java
@@ -5,17 +5,23 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         if (args.length != 2) {
+            // Exactly two arguments must be given
             argumentsError();
-            System.exit(1);
+
         }
         String name = args[0];
         BigDecimal yearlyIncome = null;
+        // Check if the second argument can be parsed as a BigDecimal
         try {
             yearlyIncome = new BigDecimal(args[1]);
+            if (yearlyIncome.compareTo(BigDecimal.ZERO) < 0) {
+                // The number parsed was negative. In some contexts a negative income might be acceptable, consult with BA
+                System.err.printf("%s is negative! Yearly income must be positive.\n", yearlyIncome);
+                argumentsError();
+            }
         } catch (NumberFormatException e) {
             System.err.printf("Could not parse %s as a BigDecimal/number.\n", args[1]);
             argumentsError();
-            System.exit(1);
         }
 
         // Note that due to the parameter sets in the division method in monthlyAccountFromYearly, all values will be given with two decimal places
@@ -29,5 +35,6 @@ public class Main {
     private static void argumentsError() {
         System.err.println("Exactly two arguments must be provided - a name, as a string, and a number representing yearly income.");
         System.err.println("e.g. 'java -jar payslip_calculator.jar \"Mary Song\" 60000'");
+        System.exit(1);
     }
 }
